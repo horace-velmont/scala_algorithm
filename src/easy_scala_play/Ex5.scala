c5ade35b73719674ce05d6bc780c6e962c512b1b
Horace Velmont <dragond7@naver.com>
2019-06-07 23:16:21 +0900

    6/7 play scala

--- src/easy_scala_play/Ex5.scala
+++ src/easy_scala_play/Ex5.scala
@@ -29,6 +29,68 @@ object Ex_5_5 {
   }
 
   def go(thisYear: Int, string: String) = {
-    println(string+":"+thisYear)
+    println(string + ":" + thisYear)
+  }
+}
+
+object Ex_5_6 {
+  def main(args: Array[String]): Unit = {
+    val g = f _
+    println(f(1))
+    println(g(2))
+  }
+
+  def f(i: Int) = i
+}
+
+object Ex_5_7 {
+  def main(args: Array[String]): Unit = {
+    val result = calc(x => x * x, 2, 5)
+    println(result)
+  }
+
+  def calc(f: Int => Int, start: Int, end: Int) = {
+    def loop(index: Int, sum: Int): Int = {
+      if (index > end) sum
+      else loop(index + 1, f(index) + sum)
+    }
+
+    loop(start, 0)
+  }
+}
+
+object Ex_5_8 {
+  def main(args: Array[String]): Unit = {
+    printlnStrings("string1", "string2", "string3")
+  }
+
+  def printlnStrings(args: String*) = {
+    for (arg <- args) {
+      println(arg)
+    }
+  }
+}
+
+object Ex_5_9 {
+  def main(args: Array[String]): Unit = {
+    println("기본값은 " + default())
+  }
+
+  def default(a: Int = 4, b: Int = 5): Int = a + b
+}
+
+object Ex_5_10 {
+
+  class SomeClass {
+    def apply(m: Int) = method(m)
+
+    def method(i: Int) = i + i
+
+    def method2(s: String) = s
+  }
+
+  def main(args: Array[String]): Unit = {
+    val something = new SomeClass
+    println(something(2))
   }
 }
